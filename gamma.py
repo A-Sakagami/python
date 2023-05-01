694fcf0f27757712a880742d1022b59c0129c30e
A-Sakagami <akiyasu.sakagami@gmail.com>
2023-05-01 22:14:57 +0900

    gamma

---
+++ gamma.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import gamma
+
+x = np.linspace(-5, 5, 100)
+y = gamma(x)
+
+plt.plot(x, y)
+plt.xlabel("x")
+plt.ylabel("Gamma(x)")
+plt.title("Gamma Function")
+plt.grid()
+plt.show()
